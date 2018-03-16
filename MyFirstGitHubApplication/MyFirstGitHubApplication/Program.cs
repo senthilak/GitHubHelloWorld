c5617d927a8b59a8ddaedc03ae8e1cbdec296e38
senthilak <37395913+senthilak@users.noreply.github.com>
2018-03-16 04:58:27 +0000

    Update Program.cs

--- MyFirstGitHubApplication/MyFirstGitHubApplication/Program.cs
+++ MyFirstGitHubApplication/MyFirstGitHubApplication/Program.cs
@@ -6,7 +6,7 @@ namespace MyFirstGitHubApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Edited");
         }
     }
 }
